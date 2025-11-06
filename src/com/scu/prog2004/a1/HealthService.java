69dd5e0dc53156c2171ceaa1c21f05b6c615fa48
Qiran-Hao <q.hao.11@student.scu.edu.au>
2025-11-06 22:34:20 +0800

    feat: 实现HealthService接口（服务规范抽象）

--- src/com/scu/prog2004/a1/HealthService.java
+++ src/com/scu/prog2004/a1/HealthService.java
@@ -1,4 +1,19 @@
 package com.scu.prog2004.a1;
 
+/**
+ * The health service interface defines the service capabilities that all practitioners must achieve.
+ * Embody the principle of abstraction and unify the standards for service description and emergency service judgment.
+ */
 public interface HealthService {
+    /**
+     * Description of the provided service content (such as "general medical services")
+     * @return Service description string (non-empty)
+     */
+    String provideServiceDescription();
+
+    /**
+     * Determine whether emergency services are provided (such as by cardiologists but not by general practitioners)
+     * @return true=Provide emergency services；false=Not provided
+     */
+    boolean isEmergencyServiceAvailable();
 }
